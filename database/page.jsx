5ca0a73f750cfb9979dc3e4924662d284556dcc2
UT151 <unnati.od.151@gmail.com>
2025-03-27 21:48:22 +0530

    added mongo url

--- database/page.jsx
+++ database/page.jsx
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectToDB = async () => {
     try {
         await mongoose.connect(
-            "mongodb+srv://unnatin:YeJr1@cluster0.duoyqgt.mongodb.net/"
+            process.env.MONGO_URL
         );
         console.log("Connected to MongoDB");
     } catch (error) {
